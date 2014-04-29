58831758fe981e3a4db7054aeb2693ece85cf1ea
Julian C. Dunn <jdunn@getchef.com>
2014-04-28 22:07:04 -0400

    Update metadata for release

--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,12 @@
 name             'oracle-xe'
 maintainer       'Chef Software, Inc.'
-maintainer_email 'jdunn@opscode.com'
+maintainer_email 'jdunn@getchef.com'
 license          'Apache 2.0'
-description      'Installs Oracle Express Edition'
+description      'Installs Oracle 11g Express Edition'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-%w(redhat centos).each do |p|
+%w(redhat centos scientific oracle).each do |p|
   supports p
 end
 
